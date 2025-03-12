11345e306970e56b4c46488870bfc724870aa3a8
@win <edwinzelf@me.com>
2025-03-12 16:00:02 +0100

    Update proxmox_zsh.sh

--- proxmox_zsh.sh
+++ proxmox_zsh.sh
@@ -4,7 +4,7 @@
 
 # Update packages, install essentials
 apt update
-apt install -y build-essential git zsh htop
+apt install -y build-essential git zsh htop bat
 
 # Install: oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -25,10 +25,22 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/TwoPizza9621536/zsh-eza.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/eza
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+wget -q https://github.com/edwinzelf/stuff/raw/refs/heads/main/p10k.zsh -O ~/.p10k.zsh
 
+# Set micro colorsheme
+mkdir -p ~/.config/micro/colorschemes
+wget -q https://github.com/catppuccin/micro/raw/refs/heads/main/src/catppuccin-mocha.micro -O ~/.config/micro/colorschemes/catppuccin-mocha.micro 
+cat > ~/.config/micro/settings.json <<EOF
+{
+    "colorscheme": "catppuccin-mocha"
+}
+EOF
+# done
 
 
 
 # Add to .zshrc:
 # plugins=(git zsh-syntax-highlighting zsh-autosuggestions eza)
 # ZSH_THEME="powerlevel10k/powerlevel10k" 
+# export BAT_THEME=Dracula
+# export MICRO_TRUECOLOR=1
