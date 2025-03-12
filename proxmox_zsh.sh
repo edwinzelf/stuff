832a80c6bc544669d6d08fba4b991df6f889cdde
@win <edwinzelf@me.com>
2025-03-12 15:18:08 +0100

    Update proxmox_zsh.sh

--- proxmox_zsh.sh
+++ proxmox_zsh.sh
@@ -9,3 +9,26 @@ apt install -y build-essential git zsh htop
 # Install: oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+# Add EZA to proxmox
+apt update
+apt install -y gpg
+
+mkdir -p /etc/apt/keyrings
+wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc |  gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" |  tee /etc/apt/sources.list.d/gierens.list
+chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+apt update
+apt install -y eza
+
+# Add ohmyzsh plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/TwoPizza9621536/zsh-eza.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/eza
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+
+
+
+# Add to .zshrc:
+# plugins=(git sh-eza zsh-syntax-highlighting zsh-autosuggestions eza)
+# ZSH_THEME="powerlevel10k/powerlevel10k" 
